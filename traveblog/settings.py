2098420993616bc6acbe2364ca2919104fc59ffd
Ubuntu <ubuntu@ip-172-31-17-203.us-west-2.compute.internal>
2019-10-28 12:26:07 +0000

    add index page

--- traveblog/settings.py
+++ traveblog/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'traveblog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
